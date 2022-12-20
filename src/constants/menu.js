ac3eb4bd00041a5eabd51025fcc12fa07e1ce979
krola-bit <krola310@gmail.com>
2022-12-20 20:33:00 +0100

    api kész

---
+++ src/constants/menu.js
@@ -0,0 +1,72 @@
+import { adminRoot } from "./config";
+import { UserRole } from "../utils/auth.roles";
+
+const data = [{
+  id: "basis",
+  icon: "",
+  label: "menu.basis",
+  to: `${adminRoot}/basis`,
+  subs: [{
+    icon: "",
+    label: "menu.start",
+    to: `${adminRoot}/basis/start`,
+    // roles: [UserRole.Admin, UserRole.Editor],
+  },
+  ]
+},
+{
+  id: "second-menu",
+  icon: "",
+  label: "menu.second-menu",
+  to: `${adminRoot}/second-menu`,
+  subs: [{
+    icon: "",
+    label: "menu.second",
+    to: `${adminRoot}/second-menu/second`,
+  },
+  ]
+},
+{
+  id: "pages",
+  icon: "",
+  label: "menu.pages",
+  to: "/user/login",
+  subs: [{
+    icon: "",
+    label: "menu.login",
+    to: "/user/login",
+    newWindow: true
+  }, {
+    icon: "",
+    label: "menu.register",
+    to: "/user/register",
+    newWindow: true
+  }, {
+    icon: "",
+    label: "menu.forgot-password",
+    to: "/user/forgot-password",
+    newWindow: true
+  },
+  {
+    icon: "",
+    label: "menu.reset-password",
+    to: "/user/reset-password",
+    newWindow: true
+  }
+  ]
+},
+{
+  id: "single",
+  icon: "",
+  label: "menu.single",
+  to: `${adminRoot}/single`,
+},
+{
+  id: "docs",
+  icon: "",
+  label: "menu.docs",
+  to: "https://piaf-vue-docs.coloredstrategies.com/",
+  newWindow: true
+}
+];
+export default data;

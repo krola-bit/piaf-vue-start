766e5968049cc706645b968e29347c0e3af6573d
krola_bit <krola310@gmail.com>
2023-04-09 10:50:24 +0200

    04,09,2

--- src/views/app/ajanlat/tetel.vue
+++ src/views/app/ajanlat/tetel.vue
@@ -28,7 +28,7 @@
               <div class="col-9 v1">
                 <b-card-text class="col-2 v2">
                   <b-form-input v-model="filteredAlapzsalu.mennyiseg" placeholder="0"
-                    :class="{ 'bg-success': filteredAlapzsalu.mennyiseg > 0 }" ref="inputRef" />
+                    :class="{ 'bg-success': filteredAlapzsalu.mennyiseg > 0 }"/>
                   {{ filteredAlapzsalu.mertekegyseg }}
                 </b-card-text>
                 <b-card-text class="col-2 v2">{{ filteredAlapzsalu.anyagegysegar }} Ft</b-card-text>
@@ -59,16 +59,13 @@
 
 <script>
 import router from "../../../router";
-import { ref } from "vue";
+
 
 const item = "alapZsalu";
 
 export default {
   name: "Colop",
 
-  setup() {
-    const inputRef = ref(null);
-  },
 
   data() {
     return {
@@ -88,10 +85,6 @@ export default {
       this.alapZsalu = JSON.parse(localStorage.getItem("alapZsalu"));
       this.filterAlapZsalu();
     }
-    const focusInput = () => {
-      this.$refs.inputRef.focus();
-    };
-    this.$nextTick(focusInput);
   },
 
   methods: {
